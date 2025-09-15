4bd37f4bcbfe7ce5d10907214b9f9c9bbfbc6d7e
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-09-15 18:28:11 +0200

    changes

--- 1º Trimestre/TEMA1/Ejercicio7.java
+++ 1º Trimestre/TEMA1/Ejercicio7.java
@@ -1,52 +1,62 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Ejercicio7 {
 
-    public static void Ej7(String nomArchivo, char opcion) {
+    public static void Ej7_n(String nomArchivo, String opcion) {
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Escribe el nº del archivo: ");
+        nomArchivo = sc2.nextLine();
+        System.out.println("Dame una opcion:");
+        opcion = sc2.nextLine();
+        sc2.close();
+        System.out.println("Opcion seleccionada:" + opcion);
 
-        do {
-            System.out.println("Opcion seleccionada:" + opcion);
-            switch (opcion) {
-                case 'n':
-                try (Scanner sc = new Scanner(new File(nomArchivo))){
+        switch (opcion) {
+            case "n":
+                try (Scanner sc = new Scanner(new File(nomArchivo))) {
+
                     int numLineas = 0;
-                    String[] palabras;
+                    int numPalabras = 0;
+
                     while (sc.hasNextLine()) {
+                        String linea = sc.nextLine();
                         numLineas++;
-                        if (sc.nextLine().contains(" ")) {
-                            
-                        }
+                        String[] palabras = linea.split("\\W+");
+                        numPalabras += palabras.length;
                     }
+                    
                     System.out.println("Numero de lineas del archivo:" + numLineas);
-                } catch (Exception e) {
+                    System.out.println("Numero palabras: " + numPalabras);
+
+                } catch (IOException e) {
                     System.out.println("error");
                 }
 
-                    break;
-                case 'A':
+                break;
+            case "A":
 
-                    break;
+                break;
 
-                case 'D':
+            case "D":
 
-                    break;
+                break;
 
-                case 'a':
+            case "a":
 
-                    break;
+                break;
 
-                case 'd':
+            case "d":
 
-                    break;
+                break;
 
-                default:
-                    break;
-            }
-        } while (opcion != 'e');
+            default:
+                break;
+        }
     }
 
     public static void main(String[] args) {
-
+        Ej7_n("dos.txt", "n");
     }
 }
