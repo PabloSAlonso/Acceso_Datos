88a17c316612bb25b93c145486ec789732a7e8fd
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-09-16 13:00:01 +0200

    changes

--- 1º Trimestre/TEMA1/Ejercicio7.java
+++ 1º Trimestre/TEMA1/Ejercicio7.java
@@ -2,16 +2,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Ejercicio7 {
-    // static ArrayList<String> lineas = new ArrayList<>();
 
-    public static void escribirArchivo(String nomArchivo, String cadena) throws IOException {
+    static ArrayList<String> lineas;
+
+    public static void escribirArchivo(String nomArchivo, String archivoBase, ArrayList<String> lineas)
+            throws IOException {
         try (FileWriter fichOut = new FileWriter(nomArchivo)) {
-            for (int i = 0; i < cadena.length(); i++) {
-                fichOut.write(cadena.charAt(i));
+            for (String linea : lineas) {
+                fichOut.write(linea);
+                fichOut.write(System.getProperty("line.separator"));
             }
+            System.out.println("Archivo: " + nomArchivo + " creado a partir de " + archivoBase);
         } catch (IOException e) {
             System.out.println("error");
         }
@@ -22,9 +28,10 @@ public class Ejercicio7 {
         try (Scanner sc = new Scanner(new File(nomArchivo))) {
             int numLineas = 0;
             int numPalabras = 0;
+            lineas = new ArrayList<>();
             while (sc.hasNextLine()) {
                 String linea = sc.nextLine();
-                // lineas.add(linea);
+                lineas.add(linea);
                 numLineas++;
                 palabras += linea.split("\\W+");
                 numPalabras += palabras.length();
@@ -37,7 +44,7 @@ public class Ejercicio7 {
         return palabras;
     }
 
-    public static void Ej7_n(String nomArchivo, String opcion) throws IOException {
+    public static void Menu(String nomArchivo, String opcion) throws IOException {
         // Scanner sc2 = new Scanner(System.in);
         // System.out.println("Escribe el nº del archivo: ");
         // nomArchivo = sc2.nextLine();
@@ -54,23 +61,29 @@ public class Ejercicio7 {
                 break;
 
             case "A":
-                escribirArchivo("holaa.txt", leerArchivo("ejemplo.txt"));
-                
+                leerArchivo(nomArchivo);
+                Collections.sort(lineas);
+                escribirArchivo("holaa.txt", "ejemplo.txt", lineas);
+
                 break;
 
             case "D":
-                escribirArchivo("holaa.txt", leerArchivo("ejemplo.txt"));
+                leerArchivo(nomArchivo);
+                Collections.sort(lineas, Collections.reverseOrder());
+                escribirArchivo("holaa.txt", "ejemplo2.txt", lineas);
 
                 break;
 
             case "a":
-                escribirArchivo("holaa.txt", leerArchivo("ejemplo.txt"));
-
+                leerArchivo(nomArchivo);
+                Collections.sort(lineas, String.CASE_INSENSITIVE_ORDER);
+                escribirArchivo("holaa.txt", "ejemplo2.txt", lineas);
                 break;
 
             case "d":
-                escribirArchivo("holaa.txt", leerArchivo("ejemplo.txt"));
-
+                leerArchivo(nomArchivo);
+                Collections.sort(lineas, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
+                escribirArchivo("holaa.txt", "ejemplo2.txt", lineas);
                 break;
 
             default:
@@ -79,6 +92,6 @@ public class Ejercicio7 {
     }
 
     public static void main(String[] args) throws IOException {
-        Ej7_n("ejemplo.txt", "n");
+        Menu("ejemplo.txt", "A");
     }
 }
