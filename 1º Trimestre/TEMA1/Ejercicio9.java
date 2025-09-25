e282ccb5a5d24480b0864cd539a3d7432b285251
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-09-25 10:43:41 +0200

    changes

--- 1º Trimestre/TEMA1/Ejercicio9.java
+++ 1º Trimestre/TEMA1/Ejercicio9.java
@@ -1,5 +1,9 @@
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,12 +11,12 @@ import java.util.Scanner;
 
 public class Ejercicio9 {
 
-    public static void altaAlumnos(File fichero, ArrayList<Alumno> datos) {
+    public static void altaAlumnos(String fichero, ArrayList<Alumno> datos) throws IOException {
         Alumno alumno = new Alumno();
         FileOutputStream fos = null;
         DataOutputStream out = null;
         try {
-            fos = new FileOutputStream(fichero);
+            fos = new FileOutputStream(new File(fichero), true);
             out = new DataOutputStream(fos);
             for (Alumno al : datos) {
                 out.writeInt(al.getCodigo());
@@ -20,6 +24,7 @@ public class Ejercicio9 {
                 out.writeFloat(al.getAltura());
             }
             datos.add(alumno);
+
         } catch (IOException e) {
             System.out.println("Error: " +
                     e.getLocalizedMessage());
@@ -30,47 +35,61 @@ public class Ejercicio9 {
                 fos.close();
         }
     }
-    public static void consultarAlumnos(){
 
+    public static void consultarAlumnos(ArrayList<Alumno> datos) throws IOException {
+        FileInputStream fis = new FileInputStream("alumnos.dat");
+        try (DataInputStream dis = new DataInputStream(fis)) {
+            while (true) {
+                dis.readInt();
+                dis.readUTF();
+                dis.readFloat();
+            }
+
+        } catch (EOFException e) {
+            System.out.println("Fin de fichero");
+        }
     }
 
-}
+    public static void modificarAlumnos(int codigoAlumno) {
+        
+    }
 
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    ArrayList<Alumno> listaAlumnos = new ArrayList<>();
-    int opcion;
-    do {
-        System.out.println("Elige una opcion");
-        System.out.println("1.- Dar de alta alumnos");
-        System.out.println("2.- Consultar alumnos");
-        System.out.println("3.- Modificar alumnos");
-        System.out.println("4.- Borrar alumnos");
-        System.out.println("5.- SALIR");
-        opcion = sc.nextInt();
-        switch (opcion) {
-            case 1:
-                // Código opcion 1
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("alumnos.dat"));
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        int opcion;
+        do {
+            System.out.println("Elige una opcion");
+            System.out.println("1.- Dar de alta alumnos");
+            System.out.println("2.- Consultar alumnos");
+            System.out.println("3.- Modificar alumnos");
+            System.out.println("4.- Borrar alumnos");
+            System.out.println("5.- SALIR");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    // Código opcion 1
+                    altaAlumnos("alumnos.dat", listaAlumnos);
+                    break;
+                case 2:
+                    // Código opcion 2
 
-                break;
-            case 2:
-                // Código opcion 2
+                    break;
+                case 3:
+                    // Código opcion 3
 
-                break;
-            case 3:
-                // Código opcion 3
+                    break;
+                case 4:
+                    // Código opcion 4
 
-                break;
-            case 4:
-                // Código opcion 4
-
-                break;
-            case 5:
-                // Código opcion 5
-                System.out.println("Chaaaaao");
-                break;
-            default:
-                System.out.println("Opcion no valida");
-        }
-    } while (opcion != 5);
+                    break;
+                case 5:
+                    // Código opcion 5
+                    System.out.println("Chaaaaao");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        } while (opcion != 5);
+    }
 }
