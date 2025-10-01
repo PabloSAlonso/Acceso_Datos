910c4ebc5761cdbb57b0138795b26a450c07b63d
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-01 13:57:37 +0200

    changes

--- 1º Trimestre/TEMA2/creacionDom.java
+++ 1º Trimestre/TEMA2/creacionDom.java
@@ -1,12 +1,40 @@
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class creacionDom {
 
     public static void main(String[] args) {
+        Node peliculas, p, hijo;
+        NodeList pelicula, hijos;
         String ruta = "archivo.xml";
+        NamedNodeMap atributos;
         Document doc = creaArbol(ruta);
+        peliculas = doc.getFirstChild();
+        pelicula = peliculas.getChildNodes();
+        for (int i = 0; i < pelicula.getLength(); i++) {
+            p = pelicula.item(i);
+            if(p.getNodeType() == Node.ELEMENT_NODE){
+                System.out.println(p.getNodeName());
+                hijos = p.getChildNodes();
+                if (p.hasAttributes()){
+                    atributos = p.getAttributes();
+                    for (int j = 0; j < atributos.getLength(); j++) {
+                        System.out.println("ATRIBUTOS: " + atributos.item(j).getNodeName() + " - " + atributos.item(j).getNodeValue());
+                    }
+                }
+                for (int j = 0; j < hijos.getLength(); j++) {
+                    hijo = hijos.item(j);
+                    if (hijo.getNodeType() == Node.ELEMENT_NODE){
+                        System.out.println(hijo.getNodeName() + " - " + hijo.getFirstChild().getNodeValue());
+                    }
+                }
+            }
+        }
+
     }
 
     public static Document creaArbol(String ruta) {
