4f78615d4eff12fc6ace1b2576ecf9da4de6e6cf
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-10-20 18:10:49 +0200

    changes

--- 1º Trimestre/TEMA2/SAX/BoletinSAX3.java
+++ 1º Trimestre/TEMA2/SAX/BoletinSAX3.java
@@ -2,66 +2,50 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class BoletinSAX3 extends DefaultHandler{
+public class BoletinSAX3 extends DefaultHandler {
+    String contenido = "";
     boolean flag = false;
-    String titulo = "";
+    int numero = 1;
     int cont = 0;
+
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        // TODO Auto-generated method stub
         super.characters(ch, start, length);
-        titulo = new String(ch, start, length);
+        contenido = new String(ch, start, length);
         if (flag) {
-            System.out.print(titulo);
+            System.out.printf("%s.",contenido);
             flag = false;
         }
     }
 
     @Override
-    public void endDocument() throws SAXException {
-        // TODO Auto-generated method stub
-        super.endDocument();
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        System.out.println("EJERCICIO 16");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (qName.equals("director")) {
+            cont++;
+            if (cont > numero) {
+                flag = true;
+                System.out.printf("%s: ", qName);
+            }
+        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        // TODO Auto-generated method stub
         super.endElement(uri, localName, qName);
-        if (qName == "pelicula"){
+        if(qName.equals("pelicula")){
             cont = 0;
         }
     }
 
     @Override
-    public void startDocument() throws SAXException {
-        // TODO Auto-generated method stub
-        super.startDocument();
-        System.out.println("EJERCICIO 14");
-    }
-
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        // TODO Auto-generated method stub
-        super.startElement(uri, localName, qName, attributes);
-        if (qName.equals("filmoteca")) {
-            System.out.printf("<%s>\n", qName);
-        } else if (qName.equals("pelicula") ) {
-            System.out.printf("\t<%s ", qName);
-            for (int i = 0; i < attributes.getLength(); i++) {
-                System.out.print(attributes.getLocalName(i) + " - " + attributes.getValue(i));
-            }
-            System.out.println(">");
-        } else if (qName.equals("titulo")) {
-            flag = true;
-            System.out.printf("\t\t<%s> ", qName);
-        } else if (qName.equals("director")) {
-            System.out.printf("\t<%s>\n", qName);
-        } else if (qName.equals("nombre")) {
-            flag = true;
-            System.out.printf("\t\t\t<%s> ", qName);
-        } else if (qName.equals("apellido")) {
-            flag = true;
-            System.out.printf("\t\t\t<%s> ", qName);
-        }
+    public void endDocument() throws SAXException {
+        super.endDocument();
     }
 }
