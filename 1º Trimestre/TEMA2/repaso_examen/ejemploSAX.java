c4a616aee55285f4bff659986f05f1be1d98e904
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-11-17 18:24:30 +0100

    changes

--- 1º Trimestre/TEMA2/repaso_examen/ejemploSAX.java
+++ 1º Trimestre/TEMA2/repaso_examen/ejemploSAX.java
@@ -1,5 +1,39 @@
 package repaso_examen;
 
-public class ejemploSAX {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ejemploSAX extends DefaultHandler  {
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        // TODO Auto-generated method stub
+        super.characters(ch, start, length);
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        // TODO Auto-generated method stub
+        super.endDocument();
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        // TODO Auto-generated method stub
+        super.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        // TODO Auto-generated method stub
+        super.startDocument();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        // TODO Auto-generated method stub
+        super.startElement(uri, localName, qName, attributes);
+    }
     
 }
