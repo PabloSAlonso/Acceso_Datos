68d1190926724bb3a199ebcddfe267a4c9c556e6
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-20 10:43:25 +0100

    changes

--- 1º Trimestre/TEMA5/tema5maven/src/main/java/ejem1/Personas.java
+++ 1º Trimestre/TEMA5/tema5maven/src/main/java/ejem1/Personas.java
@@ -2,8 +2,12 @@ package ejem1;
 
 import java.util.ArrayList;
 
+import javax.print.attribute.standard.Media;
+
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -11,6 +15,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/personas")
 public class Personas {
@@ -25,7 +30,7 @@ public class Personas {
 
     // Ejercicio 2
     @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public ArrayList<Persona> listar() {
         return personas;
     }
@@ -48,10 +53,10 @@ public class Personas {
     @GET
     @Path("/buscar")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public String ver_2(@DefaultValue("a") @QueryParam("patron") String patron){
+    public String ver_2(@QueryParam("patron") String patron) {
         ArrayList<String> nombres = new ArrayList<>();
         for (Persona persona : personas) {
-            if (persona.getNombre().contains(patron)){
+            if (persona.getNombre().contains(patron)) {
                 nombres.add(persona.getNombre());
             }
         }
@@ -59,11 +64,58 @@ public class Personas {
     }
 
     // Ejercicio 6
+    @POST
     @Path("/form")
     @Consumes("application/x-www-form-urlencoded")
-    public void insertarPersonas(){
-        
+    public Response insertarPersonas(@FormParam("id") int id, @FormParam("nombre") String nombre,
+            @FormParam("casado") boolean casado, @FormParam("sexo") String sexo) {
+        Persona p = new Persona();
+        p.setId(id);
+        p.setNombre(nombre);
+        p.setCasado(casado);
+        p.setSexo(sexo);
+        personas.add(p);
+        return Response.ok(personas).build();
     }
 
+    // Ejercicio 7
+    @POST
+    @Path("/add")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response insertarVariasPersonas() {
+        for (Persona persona : personas) {
+            personas.add(persona);
+        }
+        return Response.ok(personas).build();
+    }
+
+    // Ejercicio 8
+    @DELETE
+    @Path("/{id}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response borrarPersonas(@PathParam("id") int id) {
+        for (int i = personas.size(); i >= 0; i--) {
+            if (personas.get(i).getId() == id) {
+                personas.remove(personas.get(i));
+            }
+        }
+        return Response.ok(personas).build();
+    }
+
+    // Ejercicio 9
+    @GET
+    @Path("/buscar")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public String ver_3(@DefaultValue("a") @QueryParam("patron") String patron) {
+        ArrayList<String> nombres = new ArrayList<>();
+        for (Persona persona : personas) {
+            if (persona.getNombre().contains(patron)) {
+                nombres.add(persona.getNombre());
+            }
+        }
+        return nombres.toString();
+    }
 
 }
